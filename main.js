c4ffbb4ed303de90a94bbf16f2ca613c7238a496
ARandomPotatoPerson <richardzhao2002@gmail.com>
2021-05-01 20:15:08 -0400

    Implementing Negative Integers
    This update enables the processing of negative integers. Negative integers will display as the integer with a "-" sign before it.
    
    Ex:
    1 -> One
    -1 -> -One

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ for (const ele of nums) {
 	var num = Number.parseInt(ele);
 	var segment = "";
 	if (!Number.isNaN(num)) {
+		if (num < 0) {
+			out = out + "-";
+			num = -num;
+		}		
 		while (num > 0) {
 			segment = convert[num % 10] + segment;
 			num = Math.floor(num / 10);
