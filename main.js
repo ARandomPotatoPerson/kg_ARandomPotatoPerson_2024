5ba29c960fa9e5e09e1d289258d80cf40cf3f8dd
ARandomPotatoPerson <richardzhao2002@gmail.com>
2021-05-01 20:07:54 -0400

    Implementing Whole Number Reading
    This update adds a basic ability to read whole numbers, skipping over non-numerical entries, before appending them all to a final output string and printing the String. However, this update cannot properly process negative integers.

---
+++ main.js
@@ -0,0 +1,18 @@
+var nums = process.argv.slice(2);
+var convert = ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
+
+var out = "";
+
+for (const ele of nums) {
+	var num = Number.parseInt(ele);
+	var segment = "";
+	if (!Number.isNaN(num)) {
+		while (num > 0) {
+			segment = convert[num % 10] + segment;
+			num = Math.floor(num / 10);
+		}
+		out = out + segment + ",";
+	}
+}
+
+process.stdout.write(out.slice(0, -1))
